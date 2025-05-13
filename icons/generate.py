f60647d4c6602eb533f8badef4afdaddf89723f8
Ivan Perevala <ivan95perevala@gmail.com>
2025-05-14 01:02:15 +0200

    Update generate.py

--- icons/generate.py
+++ icons/generate.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2020-2025 Ivan Perevala <ivan95perevala@gmail.com>
+# SPDX-FileCopyrightText: 2025 Ivan Perevala <ivan95perevala@gmail.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
