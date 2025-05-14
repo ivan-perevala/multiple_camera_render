8b1a118383067b276e92f7067f242c3849220020
Ivan Perevala <ivan95perevala@gmail.com>
2025-05-14 12:27:01 +0200

    Fixed broken tests after separating dev and release import modes introduced

--- __init__.py
+++ __init__.py
@@ -2,6 +2,24 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# NOTE: This file should be used only for development of the extension, never for releases.
+# NOTE: This file should be used only for development of the extension, never for releases. Even than - here is required
+# check if package imported from within Blender. Reason is that, for example, `pytest` would try to import package, but
+# as far as there is no `bpy` module available, tests wouldn't run properly.
 
-from .src.multiple_camera_render import register, unregister
+HAS_BPY = False
+try:
+    import bpy
+except ImportError:
+    pass
+else:
+    if hasattr(bpy, "app"):
+        HAS_BPY = True
+
+if HAS_BPY:
+    from .src.multiple_camera_render import register, unregister
+else:
+    def register():
+        pass
+
+    def unregister():
+        pass
