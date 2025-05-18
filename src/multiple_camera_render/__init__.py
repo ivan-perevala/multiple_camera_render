a3eb32116d837f7191c0bc55a4b15909eda29884
Ivan Perevala <ivan95perevala@gmail.com>
2025-05-18 13:01:28 +0200

    Cleanup __init__.py file

--- src/multiple_camera_render/__init__.py
+++ src/multiple_camera_render/__init__.py
@@ -14,15 +14,19 @@ from bpy.props import PointerProperty
 from bpy.types import Scene, TOPBAR_MT_render
 from bpy.app.handlers import persistent
 
-try:
-    from ... import __package__ as ADDON_PKG
-except ImportError:
-    ADDON_PKG = __package__
+# NOTE: Development mode uses different package than release, this should be here for now.
+#
+# try:
+#     from ... import __package__ as ADDON_PKG
+# except ImportError:
+#     ADDON_PKG = __package__
+# else:
+#     if ADDON_PKG == 'bl_ext':
+#         ADDON_PKG = __package__
+
+ADDON_PKG = __package__
 
 import bhqrprt
-
-_CUR_DIR = os.path.dirname(__file__)
-DATA_DIR = os.path.join(_CUR_DIR, "data")
 
 log = logging.getLogger()
 
@@ -69,25 +73,18 @@ def handler_load_post(_=None):
     bhqrprt.log_bpy_struct_properties(log, struct=scene.mcr)
 
 
-_handlers = (
-    (bpy.app.handlers.load_post, handler_load_post),
-)
-
-
-@bhqrprt.register_reports(log, props.Preferences, directory=os.path.join(_CUR_DIR, "logs"))
+@bhqrprt.register_reports(log, props.Preferences, directory=os.path.join(os.path.dirname(__file__), "logs"))
 def register():
     _cls_register()
     Scene.mcr = PointerProperty(type=props.SceneProps)
     TOPBAR_MT_render.append(ui.additional_TOPBAR_MT_render_draw)
 
-    for handler, callback in _handlers:
-        handler.append(callback)
+    bpy.app.handlers.load_post.append(handler_load_post)
 
 
 @bhqrprt.unregister_reports(log)
 def unregister():
-    for handler, callback in reversed(_handlers):
-        handler.remove(callback)
+    bpy.app.handlers.load_post.remove(handler_load_post)
 
     icons.Icons.cache.release()
     TOPBAR_MT_render.remove(ui.additional_TOPBAR_MT_render_draw)
