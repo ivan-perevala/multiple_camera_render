51ec3c34b822369c5f261566cab81096d741c92a
Ivan Perevala <ivan95perevala@gmail.com>
2025-08-06 23:45:43 +0200

    Test cases for added per-camera attributes

--- src/bl_tests/test_per_camera_all.py
+++ src/bl_tests/test_per_camera_all.py
@@ -13,63 +13,109 @@ if TYPE_CHECKING:
 
 
 DATA = {
-    "render.resolution_x": (128, 256, 512),
-    "render.resolution_y": (512, 256, 128),
-    "render.resolution_percentage": (10, 20, 30),
-    #
-    "render.pixel_aspect_x": (1.0, 2.0, 3.0),
+    'cycles.sampling_pattern': ('AUTOMATIC', 'TABULATED_SOBOL', 'BLUE_NOISE'),
     'render.pixel_aspect_y': (3.0, 2.0, 1.0),
-    #
-    "render.use_border": (True, False, True),
-    "render.use_crop_to_border": (False, True, False),
-    #
-    "render.fps": (30, 60, 90),
-    "render.fps_base": (0.5, 0.7, 1.0),
-    #
-    "render.use_motion_blur": (True, False, False),
-    "render.motion_blur_position": ('START', 'CENTER', 'END'),
-    "render.motion_blur_shutter": (0.1, 0.5, 0.9),
-    #
-    "render.use_simplify": (True, False, True),
-    "render.simplify_subdivision": (3, 4, 5),
-    "render.simplify_child_particles": (0.1, 0.2, 0.3),
-    "render.simplify_volumes": (0.1, 0.2, 0.3),
-    "render.simplify_subdivision_render": (20, 30, 40),
-    ##
-    "view_settings.view_transform": ('AgX', 'Standard', 'Filmic'),
-    "view_settings.look": ('None', 'Medium Contrast', 'High Contrast'),
-    "view_settings.exposure": (0.1, 0.5, 1.0),
-    "view_settings.gamma": (1.0, 1.5, 2.0),
-    ##
-    "cycles.use_adaptive_sampling": (True, False, True),
-    "cycles.samples": (64, 128, 256),
-    "cycles.time_limit": (1.1, 2.2, 3.3),
-    #
-    # 'OPTIX' is omitted as far as its not supported for GitHub actions
-    "cycles.denoiser": ('OPENIMAGEDENOISE', 'OPENIMAGEDENOISE', 'OPENIMAGEDENOISE'),
-    #
-    "cycles.use_denoising": (False, True, False),
-    "cycles.denoising_input_passes": ('RGB', 'RGB_ALBEDO', 'RGB_ALBEDO_NORMAL'),
-    #
-    "cycles.max_bounces": (12, 6, 3),
-    "cycles.diffuse_bounces": (1, 2, 3),
-    "cycles.glossy_bounces": (4, 5, 6),
-    "cycles.transmission_bounces": (0, 1, 4),
-    "cycles.volume_bounces": (2, 1, 9),
-    "cycles.transparent_max_bounces": (4, 2, 1),
-    #
+    "cycles_curves.shape": ('RIBBONS', 'THICK', 'RIBBONS'),
+    "cycles_curves.subdivisions": (1, 2, 3),
+    "cycles.adaptive_min_samples": (10, 20, 30),
+    "cycles.adaptive_threshold": (0.01, 0.03, 0.07),
+    "cycles.ao_bounces_render": (4, 5, 6),
+    "cycles.ao_bounces": (0, 1, 3),
+    "cycles.auto_scrambling_distance": (True, False, True),
     "cycles.blur_glossy": (0.1, 0.2, 0.3),
-    #
+    "cycles.camera_cull_margin": (0.2, 0.5, 0.7),
     "cycles.caustics_reflective": (True, False, True),
     "cycles.caustics_refractive": (False, True, False),
-    "cycles.use_fast_gi": (True, False, False),
+    "cycles.debug_use_compact_bvh": (True, False, True),
+    "cycles.debug_use_spatial_splits": (True, False, True),
+    # 'OPTIX' is omitted as far as its not supported for GitHub actions.
+    "cycles.denoiser": ('OPENIMAGEDENOISE', 'OPENIMAGEDENOISE', 'OPENIMAGEDENOISE'),
+    "cycles.denoising_input_passes": ('RGB', 'RGB_ALBEDO', 'RGB_ALBEDO_NORMAL'),
+    "cycles.denoising_prefilter": ('NONE', 'FAST', 'ACCURATE'),
+    "cycles.denoising_quality": ('HIGH', 'BALANCED', 'FAST'),
+    "cycles.denoising_use_gpu": (True, False, True),
+    "cycles.diffuse_bounces": (1, 2, 3),
+    "cycles.distance_cull_margin": (50, 70, 80),
     "cycles.fast_gi_method": ('REPLACE', 'ADD', 'ADD'),
-    "cycles.ao_bounces": (0, 1, 3),
-    "cycles.ao_bounces_render": (4, 5, 6),
-    #
-    "cycles.texture_limit": ('128', '256', '512'),
+    "cycles.film_exposure": (1.0, 1.1, 1.2),
+    "cycles.film_transparent_glass": (True, False, True),
+    "cycles.film_transparent_roughness": (0.1, 0.2, 0.3),
+    "cycles.filter_width": (1.6, 1.8, 2.0),
+    "cycles.glossy_bounces": (4, 5, 6),
+    "cycles.guiding_training_samples": (0, 128, 256),
+    "cycles.light_sampling_threshold": (0.01, 0.03, 0.08),
+    "cycles.max_bounces": (12, 6, 3),
+    "cycles.min_light_bounces": (0, 1, 2),
+    "cycles.min_transparent_bounces": (0, 1, 2),
+    "cycles.pixel_filter_type": ('BOX', 'GAUSSIAN', 'BLACKMAN_HARRIS'),
+    "cycles.preview_scrambling_distance": (True, False, True),
+    "cycles.rolling_shutter_duration": (0.1, 0.3, 0.8),
+    "cycles.rolling_shutter_type": ('NONE', 'TOP', 'NONE'),
+    "cycles.sample_clamp_direct": (0.0, 0.5, 1.0),
+    "cycles.sample_clamp_indirect": (0.1, 0.2, 0.3),
+    "cycles.sample_offset": (0, 1, 2),
+    "cycles.sample_subset_length": (512, 1024, 2048),
+    "cycles.samples": (64, 128, 256),
+    "cycles.scrambling_distance": (0.1, 0.2, 0.3),
+    "cycles.seed": (0, 10, 20),
     "cycles.texture_limit_render": ('OFF', '128', '256'),
-    ##
+    "cycles.texture_limit": ('128', '256', '512'),
+    "cycles.tile_size": (64, 128, 256),
+    "cycles.time_limit": (1.1, 2.2, 3.3),
+    "cycles.transmission_bounces": (0, 1, 4),
+    "cycles.transparent_max_bounces": (4, 2, 1),
+    "cycles.use_adaptive_sampling": (True, False, True),
+    "cycles.use_auto_tile": (True, False, True),
+    "cycles.use_camera_cull": (True, False, True),
+    "cycles.use_denoising": (False, True, False),
+    "cycles.use_distance_cull": (True, False, True),
+    "cycles.use_fast_gi": (True, False, False),
+    "cycles.use_guiding": (True, False, True),
+    "cycles.use_light_tree": (True, False, True),
+    "cycles.use_sample_subset": (True, False, True),
+    "cycles.use_surface_guiding": (True, False, True),
+    "cycles.use_volume_guiding": (True, False, True),
+    "cycles.volume_bounces": (2, 1, 9),
+    "cycles.volume_max_steps": (64, 128, 512),
+    "cycles.volume_step_rate": (0.1, 1.0, 10.0),
+    "frame_end": (100, 200, 300),
+    "frame_start": (10, 20, 30),
+    "frame_step": (1, 2, 3),
+    "render.film_transparent": (True, False, True),
+    "render.fps_base": (0.5, 0.7, 1.0),
+    "render.fps": (30, 60, 90),
+    "render.motion_blur_position": ('START', 'CENTER', 'END'),
+    "render.motion_blur_shutter": (0.1, 0.5, 0.9),
+    "render.pixel_aspect_x": (1.0, 2.0, 3.0),
+    "render.resolution_percentage": (10, 20, 30),
+    "render.resolution_x": (128, 256, 512),
+    "render.resolution_y": (512, 256, 128),
+    "render.simplify_child_particles_render": (0.1, 0.2, 0.8),
+    "render.simplify_child_particles": (0.1, 0.2, 0.3),
+    "render.simplify_gpencil_antialiasing": (True, False, True),
+    "render.simplify_gpencil_modifier": (True, False, True),
+    "render.simplify_gpencil_onplay": (True, False, True),
+    "render.simplify_gpencil_shader_fx": (True, False, True),
+    "render.simplify_gpencil_tint": (True, False, True),
+    "render.simplify_gpencil_view_fill": (True, False, True),
+    "render.simplify_gpencil": (True, False, True),
+    "render.simplify_subdivision_render": (20, 30, 40),
+    "render.simplify_subdivision": (3, 4, 5),
+    "render.simplify_volumes": (0.1, 0.2, 0.3),
+    # NOTE: 'AUTO' is skipped because no known number of threads on test machine.
+    "render.threads_mode": ('FIXED', 'FIXED', 'FIXED'),
+    "render.threads": (1, 2, 3),
+    "render.use_border": (True, False, True),
+    "render.use_crop_to_border": (False, True, False),
+    "render.use_motion_blur": (True, False, False),
+    "render.use_persistent_data": (True, False, True),
+    "render.use_simplify": (True, False, True),
+    "view_settings.exposure": (0.1, 0.5, 1.0),
+    "view_settings.gamma": (1.0, 1.5, 2.0),
+    # NOTE: 'Standard' is always last one.
+    "view_settings.view_transform": ('AgX', 'Filmic', 'Standard'),
+    # NOTE: This is dependent from 'view_settings.view_transform', so always after it.
+    "view_settings.look": ('None', 'Medium Contrast', 'High Contrast'),
     "world": ('World', 'World.001', 'World.002'),
 }
 
@@ -126,6 +172,7 @@ def test_per_camera_all(multiple_camera_render_module, with_select_camera):
 
         for data_path, values in DATA.items():
             curr_value = scene.path_resolve(data_path)
+
             expected_value = values[index]
 
             if isinstance(curr_value, float):
